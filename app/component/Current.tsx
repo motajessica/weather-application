224ac3ed7746ef362d5009a485d252351f13e076
motajessica <motajessicaa@gmail.com>
2023-10-17 11:09:16 +1300

    Use prettier and add location icon

--- app/component/Current.tsx
+++ app/component/Current.tsx
@@ -1,40 +1,39 @@
-import React from 'react'
-import { getCurretnDate } from '../utils/currentDate'
-import { CiLocationOn } from  "react-icons/ci"
+import React from "react";
+import { getCurretnDate } from "../utils/currentDate";
+import { CiLocationOn } from "react-icons/ci";
 
-
-const Current = ( {data}: any ) => {
-  const currentDate = getCurretnDate()
-  const weatherIcon = data.current.condition.icon 
+const Current = ({ data }: any) => {
+  const currentDate = getCurretnDate();
+  const weatherIcon = data.current.condition.icon;
   return (
     <>
-    <div className="flex flex-col mb-8 md:mb-0 items-start gap-2 w-1/2">
-      <div className="flex items-center">
-        <div>
-          <h1 className="text-3xl text-white">Today</h1>
-          <p className="text-white"> {currentDate}</p>
-        </div>
+      <div className="flex flex-col mb-8 md:mb-0 items-start gap-2 w-1/2">
+        <div className="flex items-center">
+          <div>
+            <h1 className="text-3xl text-white">Today</h1>
+            <p className="text-white"> {currentDate}</p>
+          </div>
           {weatherIcon && (
-            <div> 
-              <img className='w-[50px] object-cover' src={weatherIcon} alt={data.current.condition.text} />
+            <div>
+              <img
+                className="w-[50px] object-cover"
+                src={weatherIcon}
+                alt={data.current.condition.text}
+              />
             </div>
-
           )}
+        </div>
+        <p className="text-3xl tex-white">
+          {data.current.temp_c.toFixed()}
+          <span>ºC</span>
+        </p>
+        <span className="text-white">{data.current.condition.text}</span>
+          <div className="flex items-center text-black bg-white/90 px-2 py-2 rounded-xl">
+            <CiLocationOn />
+          </div>
       </div>
-      <p className="text-3xl tex-white"> 
-        {data.current.temp_c.toFixed()}
-        <span>ºC</span>
-      </p>
-      <span className="text-white">{data.current.condition.text}</span>
-    </div>
-    <div>
-      <div className='flex items-center text-black bg-white/90 px-2 py-2 rounded-xl'>
-          < CiLocationOn />
-      </div>
-
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Current
+export default Current;
